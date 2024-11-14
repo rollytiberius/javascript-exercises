14d86da54060fe16b23658931fe2c026faca78c9
roland_work <rollytiberius@gmail.com>
2024-11-14 14:28:11 +0000

    Finished 02 exercice

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, repeatTimes) {
+  if (repeatTimes < 0) {
+    return "ERROR";
+  }
+  let repeat = repeatTimes;
+  let finalString = "";
+  while (repeat > 0) {
+    repeat -= 1;
+    finalString += string;
+  }
+  return finalString;
 };
 
 // Do not edit below this line
