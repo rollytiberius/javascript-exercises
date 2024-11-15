1624478b511eb40881fb1c69e8adb7f485b52270
roland_work <rollytiberius@gmail.com>
2024-11-15 13:37:03 +0000

    Some work on 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function (array, extraInput) {
-  return array.push(extraInput).pop();
+const removeFromArray = function(array, extraInput, oneMore = null) {
+  const arraySort = array.filter((item) => {
+    return item !== extraInput;
+  });
+
+  const arraySort2 = arraySort.filter((item) => {
+    return item !== oneMore;
+  });
+  return arraySort2;
 };
 
 // Do not edit below this line
