f848bf682349827c5f79cee15769dab22ec7a8cc
rollytiberius <rollytiberius@gmail.com>
2024-11-16 17:54:23 +0000

    Finishes exercice nr 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+	let result = false;
+	if (year % 100 === 0) {
+		if (year % 400 === 0) {
+			return true;
+		}
+		return false;
+	}
+	if (year % 4 === 0) {
+		result = true;
+	}
+	return result;
 };
 
 // Do not edit below this line
